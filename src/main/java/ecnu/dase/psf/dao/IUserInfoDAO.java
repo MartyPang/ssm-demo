89537ac2da31249ee49feacbffec995876329ed0
MartyPang <pang.shuaifeng@gmail.com>
2019-10-08 16:57:55 +0800

    Initial commit

---
+++ src/main/java/ecnu/dase/psf/dao/IUserInfoDAO.java
@@ -0,0 +1,9 @@
+package ecnu.dase.psf.dao;
+
+/**
+ * @author MartyPang
+ * @version 1.0
+ * @date 2019/10/8 16:44
+ */
+public interface IUserInfoDAO {
+}
---
+++ src/main/java/ecnu/dase/psf/service/UserInfoServiceImpl.java
@@ -0,0 +1,9 @@
+package ecnu.dase.psf.service;
+
+/**
+ * @author MartyPang
+ * @version 1.0
+ * @date 2019/10/8 16:46
+ */
+public class UserInfoServiceImpl {
+}
